f6e7e0e58ab125b9d13c4333dd26dc24c112d1ae
Daniel Huisman <daniel@huisman.me>
2020-08-31 22:32:27 +0200

    Use provided user type for me query

--- packages/auth/src/types/User.ts
+++ packages/auth/src/types/User.ts
@@ -1,13 +1,14 @@
 import {extendType} from '@nexus/schema';
 
 import {AuthContext} from '../authentication';
+import {getUserType} from '../config';
 
 export const generateTypes = () => ({
     QueryAuthUser: extendType({
         type: 'Query',
         definition(t) {
             t.field('me', {
-                type: 'User',
+                type: getUserType().name,
                 nullable: true,
                 resolve(_root, _args, context: AuthContext) {
                     return context.user;
