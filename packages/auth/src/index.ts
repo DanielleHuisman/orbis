022aff51bdbfad37d4847b102849a755f51b3b86
Daniel Huisman <daniel@huisman.me>
2022-03-28 22:35:40 +0200

    Add entities export to auth module

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,3 +2,6 @@ export * from './config';
 export * from './entities';
 export * from './module';
 export * from './providers';
+
+import * as entitiesMap from './entities';
+export const entities = Object.values(entitiesMap);
