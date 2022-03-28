a17803b728bcc8e1f2cab75bc6bd8fb9ae6bfd51
Daniel Huisman <daniel@huisman.me>
2022-03-28 23:34:36 +0200

    Add utility functions for dealing with TypeORM (#57)
    * Add utility functions for dealing with TypeORM
    
    * Remove unused @ts-expect-errors

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -1,3 +1,6 @@
+import path from 'path';
+import {readdir} from 'fs/promises';
+import {fileURLToPath} from 'url';
 import {ArgsValue, GetGen} from 'nexus/dist/core';
 import {GraphQLResolveInfo} from 'graphql';
 import {getMetadataArgsStorage, BaseEntity} from 'typeorm';
@@ -63,6 +66,25 @@ export const isGeneratedField = (field: FieldMetadata) => {
     }
 
     return false;
+};
+
+export const findEntities = (possibleEntities: (unknown[] | Record<string, unknown>)[]): Function[] =>
+    possibleEntities
+        .flatMap((possible) => typeof possible == 'object' ? Object.values(possible) : possible)
+        .filter((possibleEntity) => typeof possibleEntity === 'function') as Function[];
+
+export const findMigrations = async (url: string, name = 'migrations') => {
+    let migrations: Function[] = [];
+
+    const directory = path.dirname(fileURLToPath(url));
+    const files = await readdir(path.join(directory, name));
+
+    for (const file of files) {
+        const module = await import(path.join(directory, name, file));
+        migrations = migrations.concat(Object.values(module).filter((value) => typeof value === 'function') as Function[]);
+    }
+
+    return migrations;
 };
 
 export type Context = GetGen<'context'>;
